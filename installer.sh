cc0663ddd4e3b008b4940f6957e0ddadf6715017
YUDAMODS <119145280+YUDAMODS@users.noreply.github.com>
2024-10-24 20:12:42 +0700

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install dependensi Python
+pip install requests colorama
+
+# Install figlet dan lolcat (opsional)
+apt install figlet
+
+# install lolcat
+pip install lolcat
+
+# install python
+pkg install python
+
+# Jalankan skrip Python
+python yudamods.py
